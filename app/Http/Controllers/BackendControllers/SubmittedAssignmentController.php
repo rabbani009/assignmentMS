2a2b7dcbd1e00061df42f62fe88eba5d06357d21
unknown <farzad.edsoft@gmail.com>
2023-06-21 16:22:31 +0600

    updates

--- app/Http/Controllers/BackendControllers/SubmittedAssignmentController.php
+++ app/Http/Controllers/BackendControllers/SubmittedAssignmentController.php
@@ -14,22 +14,33 @@ class SubmittedAssignmentController extends Controller
     public function submitAssignment(Request $request)
     {
         // dd($request->all());
+
             $assignmentId = $request->input('assignment_id');
             $studentId = $request->input('student_id');
             $attachedFile = $request->file('attached_file');
+
         //If student submit assignment once then.
             $existingSubmission = SubmittedAssignment::where('assignment_id', $assignmentId)
             ->where('student_id', $studentId)
             ->first();
 
+            //if submitted once
             if ($existingSubmission) {
                 return redirect()->back()->with('warning', 'Assignment already submitted.');
+            }
+
+            //Overdue
+            $assignment = Assignment::where('assignment_id', $assignmentId)->first();
+            // dd($assignment);
+            if ($assignment->submission_date < date('Y-m-d')) {
+                return redirect()->back()->with('warning', 'Assignment submission date has passed.');
             }
         
             $submittedAssignment = new SubmittedAssignment();
             $submittedAssignment->assignment_id = $assignmentId;
             $submittedAssignment->student_id = $studentId;
             $submittedAssignment->submitted_time = date('Y-m-d');
+            
             //This is for uploaded file by student
             if ($request->hasFile('attached_file')) {
                 $image = $request->file('attached_file');
